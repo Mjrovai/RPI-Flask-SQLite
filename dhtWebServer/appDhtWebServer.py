18cc34bb9ab300bf2f6d701715ee59852bb2334f
Marcelo Rovai <Mjrovai@gmail.com>
2018-01-14 17:05:18 -0300

    Update appDhtWebServer.py

--- dhtWebServer/appDhtWebServer.py
+++ dhtWebServer/appDhtWebServer.py
@@ -17,7 +17,7 @@ import sqlite3
 
 # Retrieve data from database
 def getData():
-	conn=sqlite3.connect('sensorsData.db')
+	conn=sqlite3.connect('../sensorsData.db')
 	curs=conn.cursor()
 
 	for row in curs.execute("SELECT * FROM DHT_data ORDER BY timestamp DESC LIMIT 1"):
